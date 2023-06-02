273dba905eb0f60c6dcbd4ace89d7a2c2a15e07c
Ruri <rurik000@hotmail.com>
2023-06-02 12:47:51 -0600

    update design

--- src/app/features/home/pages/storefront/storefront.module.ts
+++ src/app/features/home/pages/storefront/storefront.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StorefrontRoutingModule } from './storefront-routing.module';
-
+import { StorefrontComponent } from './storefront.component';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
-  declarations: [],
+  declarations: [StorefrontComponent],
   imports: [
     CommonModule,
-    StorefrontRoutingModule
+    StorefrontRoutingModule,
+    DividerModule,
   ]
 })
 export class StorefrontModule { }
